dcdff35863b334e7b131570c36e94fb12372958a
ArthurOliCode <arthur.rilber@gmail.com>
2025-05-10 14:31:58 -0300

    Organizando exercícios de TPA

---
+++ TPA/Semestre-1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string nome = "Arthur";
+        string nome2 = "";
+        int idade = 0;
+        var names = new[] {"", "", "", ""};
+        Console.WriteLine("Meu nome é " + nome);
+        Console.WriteLine("Muito prazer em te conhecer! Qual é o seu nome? ");
+        nome2 = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("Muito prazer em te conhecer " + nome2 + "!");
+        if(nome2.Equals("Arthur")){
+            Console.WriteLine();
+            Console.WriteLine("Seu nome é igual ao meu!!");
+        }else{
+            
+            Console.WriteLine("Seu nome é legal!!");
+        }
+
+
+
+        
+    }
+}
