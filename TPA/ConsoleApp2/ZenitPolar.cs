a661c60fe620a67371b8c971ce45a1438748265a
ArthurOliCode <arthur.rilber@gmail.com>
2025-05-10 14:25:34 -0300

    Exercícios da matéria de TPA - Técnicas de Programação e Algoritmo

---
+++ TPA/ConsoleApp2/ZenitPolar.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq.Expressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+         string resp;
+         string frase; //String para o texto base
+        do{
+            //comandos
+            Console.WriteLine("\n\t\t--------------------------------");
+            Console.WriteLine("\t\tBem vindo ao sistema Zenit Polar");
+            Console.WriteLine("\t\t--------------------------------");
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine("Digite uma frase por favor: ");
+            frase = Console.ReadLine();
+            
+            Console.WriteLine("A criptografia do seu texto é: " + FormaMSG(frase));
+
+            //resposta para o loop
+            Console.WriteLine("\nDeseja continuar?: ");
+            resp = Console.ReadLine();
+            resp = resp[0].ToString().ToLower();
+        }while(resp.Equals("s"));
+    }
+
+    public static string Zenit(string letra){ //Método para criptografia ZenitPolar
+        letra = letra.ToLower();
+        switch(letra){
+            case "z":
+                return "p";
+            case "p":
+                 return "z";
+            case "e":
+                return "o";
+            case "o":
+                return "e";
+            case "n":
+                return "l";
+            case "l":
+                return "n";
+            case "i":
+                return "a";
+            case "a":
+                return "i";
+            case "t":
+                return "r";
+            case "r":
+                return "t";
+            case "Z":
+                return "P";
+            case "P":
+                return "Z";
+            case "E":
+	    	    return "O";
+	        case "O":
+	    	    return "E";
+	        case "N":
+	    	    return "L";
+	        case "L":
+	    	    return "N";
+	        case "I":
+	    	    return "A";
+	        case "A":
+	    	    return "I";
+	        case "T":
+	    	    return "R";
+	        case "R":
+	    	    return "T";
+            default:
+                return letra;
+        }
+    }
+
+    public static string FormaMSG(string txt){
+        string txtCript = ""; //Strinh para o texto criptografado
+        for(int i=0; i<txt.Length; i++){
+            string l = txt[i].ToString();
+            txtCript += Zenit(l);
+        }
+        return txtCript;
+    }
+}
---
+++ TPA/Semestre-1/Sorteia Duplas.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+string vFrase, x;
+int i, numsort;
+Random numAleatorio = new Random();
+
+Console.WriteLine("\n--------------------------------------");
+Console.WriteLine("Bem vindo ao Sorteia Duplas");
+Console.WriteLine("\n--------------------------------------\n\n");
+
+Console.WriteLine("Quantas duplas deseja digitar: ");
+while (!int.TryParse(Console.ReadLine(), out i) || i <= 0)
+{
+    Console.WriteLine("Número inválido. Digite novamente: ");
+}
+string[,] duplas = new string[i, 2];
+string[,] sort = new string[i, 2];
+
+
+Console.WriteLine("Digite os nomes: ");
+for(int l=0; l<i; l++)
+{
+    for(int c=0; c<2; c++)
+    {
+        Console.WriteLine($"Digite o {l+1}° nome: ");
+        vFrase = Console.ReadLine();
+        duplas[l,c] += vFrase;
+    }
+}
+for(int l=0; l<i; i++)
+{
+    for(int c=0; c<2; c++)
+    {
+        numsort = numAleatorio.Next(l,c);
+        string temp = duplas[l,c];
+        sort[l, c] = duplas[numsort,c];   
+ 
+    }
+}
+
+for (int l = 0; l < i; l++)
+{
+    for (int c = 0; c < 2; c++)
+    {
+        Console.Write($"Dupla: 1° nome - {sort[l, c]}");
+    }
+}
